93342d0c460af1884cbe6d8195313ca106ec4c52
Maks22242 <96117529+Maks22242@users.noreply.github.com>
2023-03-17 21:17:34 +0300

    Add files via upload

---
+++ Lab2/2.1.1.rb
@@ -0,0 +1,68 @@
+class Student 
+
+	def initialize(name,midleName,lastName)
+		@id
+		@lastName = lastName
+		@name = name
+		@midleName = midleName
+		@phone
+		@tme
+		@mail
+		@git
+	end
+	
+	def getid
+		@id
+	end
+	def setid(id)
+		@id = id
+	end
+	
+	def getname
+		@name
+	end
+	def setname(name)
+		@name = name
+	end
+	
+	def getmidleName
+		@midleName
+	end
+	def setmidleName(midleName)
+		@midleName = midleName
+	end
+	
+	def getlastName
+		@lastName
+	end
+	def setlastName(lastName)
+		@lastName = lastName
+	end
+	
+	def getphone
+		@phone
+	end
+	def setphone(phone)
+		@phone = phone
+	end
+	
+	def gettme
+		@tme
+	end
+	def settme(tme)
+		@tme = tme
+	end
+	
+	def getmail
+		@mail
+	end
+	def setmail(mail)
+		@mail = mail
+	end
+	
+	def getgit
+		@git
+	end
+	def setgit(git)
+		@git = git
+	end
