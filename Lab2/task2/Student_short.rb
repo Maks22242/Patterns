f9219ea67f20d81099ff688c5a0773d5172ef648
Maks22242 <96117529+Maks22242@users.noreply.github.com>
2023-04-15 13:27:53 +0300

    Update Student_short.rb

--- Lab2/task2/Student_short.rb
+++ Lab2/task2/Student_short.rb
@@ -3,18 +3,21 @@ require_relative 'Student_super'
 class Student_short < Student_super
   attr_accessor :short_name, :contact
 
-  def initialize(student)
-    @id = student.id
-    @short_name = student.short_name
-    @git = student.git
-    @contact = student.set_contacts(phone: student.phone, mail: student.mail, tme: student.tme)
+  def initialize(id:, short_name: ,git: nil,contact: nil)
+    @id = id
+    @short_name = short_name
+    @git = git
+    @contact = contact
+    end
+
+  def self.iniz_from_obj(student)
+    contact = student.set_contacts(phone: student.phone, mail: student.mail, tme: student.tme)
+    Student_short.new(id:student.id, short_name:student.short_name, git:student.git, contact: contact)
   end
 
-  def self.initz_from_str(id,str)
-    student = stud2 = Student.iniz(str)
-    student.id = id
-    Student_short.new(student)
-
+  def self.initz_from_str(id:, str:)
+    contacts = str.split(",")
+    Student_short.new(id:id, short_name:contacts[0], contact: contacts[1], git:contacts[2])
   end
 
   def to_s
@@ -22,7 +25,7 @@ class Student_short < Student_super
   end
 
   def get_info
-    git_info = "#git: #{git}" unless git.nil
-    "#{short_name} git:#{git_info} contact:#{contact}"
+    git_info = "#git: #{git}," unless git.nil?
+    "#{short_name} ,#{git_info} contact:#{contact}"
   end
 end
