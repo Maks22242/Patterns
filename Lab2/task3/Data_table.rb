700042a5f3e1c2a2c3fefe8d44456ac89ac6a99c
Maks22242 <96117529+Maks22242@users.noreply.github.com>
2023-04-16 17:27:47 +0300

    Update Data_table.rb

--- Lab2/task3/Data_table.rb
+++ Lab2/task3/Data_table.rb
@@ -1,11 +1,25 @@
 class Data_table
+
   def initialize(table)
-
     @table = table
+    @row_number = table.size
+    @column_number = max_size_column
+  end
 
+  #получить элемент по номеру столбца и строки
+  def get_item(row,col)
+    return nil if row>=row_number||col>=column_number
+    table[row][col].dup
   end
 
   private
   attr_accessor :table
+  attr_writer :row_number, :column_number
 
+  #максимальный размер столбца
+  def max_size_column
+    max_size=0
+    table.each{|el| max_size=el.size if el.size>max_size }
+    max_size
+  end
 end
