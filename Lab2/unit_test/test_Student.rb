a0f86d100f84ca7c480b901b5217cf4463424515
Maks22242 <96117529+Maks22242@users.noreply.github.com>
2023-05-13 11:39:04 +0300

    Update test_Student.rb

--- Lab2/unit_test/test_Student.rb
+++ Lab2/unit_test/test_Student.rb
@@ -1,1 +1,9 @@
-1
+require 'test/unit'
+require_relative '../Students/Student'
+
+class TestStudent < Test::Unit::TestCase
+  def test_short_name
+    student = Student.iniz("Петренко, Роман, Николаевич, +79261234567 ,makcu@mail.ru,https://t.me/lrmanga/2211")
+    assert_equal('Петренко Р. Н.', student.short_name)
+  end
+end
