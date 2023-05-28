feef6de06b108992c09f320b8c882c6bfe6f9d53
Maks22242 <96117529+Maks22242@users.noreply.github.com>
2023-05-28 20:39:47 +0300

    Add files via upload

---
+++ LabStudents/db_config/mock_data/fill_student.sql
@@ -0,0 +1,7 @@
+
+insert into student(last_name, first_name, father_name, phone, telegram, email, git) values
+                                                                                         ('Тыщенко', 'Иван', 'Алексеевич', '79517532585', NULL, 'tinka_mar@mail.ru', NULL),
+                                                                                         ('Фокина', 'Светлана', 'Олеговна', NULL, 'prettykitty', 'fik_svet@bk.ru', 'fokin_svet'),
+                                                                                         ('Колокольчиков', 'Егор', 'Александрович', '79998881487', 'simark', 'kol_eg@gmail.com', NULL),
+                                                                                         ('Григарьян', 'Никита', 'Ефимович', '79180005577', 'balnik', NULL, 'nikitos_epif'),
+                                                                                         ('Петренко', 'Артур', 'Захарович', '79990805051', 'narot', 'aptyp_zah@narod.ru', 'art_zah');
